54b852008e74707d35b685a570a98719005cc9c5
Sarthak <messagetosarthak@gmail.com>
2026-06-21 12:14:38 +0530

    Insertion sort

---
+++ Revision/21stJune/insertionsort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+int main() {
+  int n; 
+  cin >> n; 
+  vector<int> arr(n); 
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i]; 
+  }
+  //starting from the second element; 
+  for(int i = 1; i < n; i++) {
+    int key = arr[i]; 
+    int j = i-1; 
+    while(j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j]; 
+      j--;
+    }
+    arr[j+1] = key; 
+  }
+
+  //print the sorted array 
+  for(int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
