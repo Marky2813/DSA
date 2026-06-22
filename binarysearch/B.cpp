e32da2d9e99dcb21c8a5e3db2870a588e6faff10
Sarthak <messagetosarthak@gmail.com>
2026-06-23 00:30:37 +0530

    Binary Search

---
+++ binarysearch/B.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+int main() {
+  int n, q;
+  cin >> n >> q; 
+  vector<long long> arr(n);
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  for(int i = 0; i < q; i++) {
+    bool found = false;
+    long long element;
+    cin >> element; 
+    int l = 0; 
+    int r = n-1;
+    while(l <= r) {
+      int mid=(l+r)/2; 
+      if(arr[mid] == element) {
+        cout << mid+1 << "\n";
+        found = true; 
+        break; 
+      } else if (arr[mid] < element) {
+        r = mid-1;
+      } else {
+        l = mid+1; 
+      }
+    }
+    if(!found) cout << -1 << "\n";
+  }
+}
