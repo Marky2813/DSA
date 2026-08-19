406e9c85f3cced7aec0d837a8a29588f24d8b2b4
Sarthak <messagetosarthak@gmail.com>
2026-08-19 12:26:36 +0530

    recursion 8

---
+++ Recursion/lastoccur.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+long long rev(int n, vector<long long>& arr, long long x) {
+  if(n==0) {
+    return -1; 
+  }
+  int smallAns = rev(n-1, arr, x);
+  if(smallAns != -1) return smallAns; 
+
+  if(arr[n-1] == x) return n-1; 
+
+  return -1; 
+}
+
+int main() {
+  int n; 
+  cin >> n;
+  vector<long long> arr(n);
+  for(int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  long long x; 
+  cin >> x; 
+  long long ans = rev(n, arr, x);
+  if(ans!=-1) {
+  cout << ans+1;
+  } else {
+  cout << ans; 
+  }
+}
