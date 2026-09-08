633f132ec0bcbf3dea0452315f4d3ea96cb2ea6f
Sarthak <messagetosarthak@gmail.com>
2026-09-09 00:38:46 +0530

    inversions

--- sorting2/D.cpp
+++ sorting2/D.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 int inversions; 
 
-void merge(vector<long long> &arr, int l, int r) {
+int merge(vector<long long> &arr, int l, int r) {
   vector<long long> temp; 
   int i = l; 
   int mid = (l+r)/2; 
   int j = mid+1;
+  int ans = 0; 
   while(i < (mid+1) && j < (r+1)) {
     if(arr[i] <= arr[j]) {
       temp.push_back(arr[i]);
       i++;
     } else {
       temp.push_back(arr[j]);
+    ans += (mid-i+1);
       j++;
       inversions++;
     }
@@ -34,16 +36,19 @@ void merge(vector<long long> &arr, int l, int r) {
     arr[k] = temp[ptr];
     ptr++;
   }
+  return ans; 
 }
 
-void f(vector<long long> &arr, int l, int r) {
+int f(vector<long long> &arr, int l, int r) {
   if(l == r) {
-    return; 
+    return 0; 
   }
   int mid = (l+r)/2;
-  f(arr, l, mid);
-  f(arr, mid+1, r);
-  merge(arr, l, r);
+
+  int ans1 = f(arr, l, mid);
+  int ans2 = f(arr, mid+1, r);
+  int ans3 = merge(arr, l, r);
+  return ans1 + ans2 + ans3;
 }
 
 
@@ -54,10 +59,6 @@ int main() {
   for(int i = 0; i < n; i++) {
     cin >> arr[i];
   }
-  f(arr, 0, n-1);
-  for(int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-  cout << inversions;
+  int ans = f(arr, 0, n-1);
+  cout << ans;
 }
